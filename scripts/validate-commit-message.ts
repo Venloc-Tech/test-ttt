9c9c682f0dae1fa13bacfc51ac63e04c07a20db7
0x303133 <132019318+shiz-ceo@users.noreply.github.com>
2025-03-19 01:34:43 +0300

    feat: Add commit-msg git hook fro validate commit messages

---
+++ scripts/validate-commit-message.ts
@@ -0,0 +1,53 @@
+import * as process from "node:process";
+
+const commitType = /(?<type>build|chore|ci|docs|feat|fix|perf|refactor|revert|style|test)/;
+const commitScope = /(?<scope>\((?:@[a-z0-9._-]+\/[a-z0-9._-]+|[a-z0-9._-]+)(?:(?:;)(?:@[a-z0-9._-]+\/[a-z0-9._-]+|[a-z0-9._-]+))*\))/;
+const commitBreaking = /(?<breaking>!)/;
+const commitSubject = /(?<subject>.*)/;
+
+const commitRegex = new RegExp(`^${commitType.source}${commitScope.source}?${commitBreaking.source}?:\\s${commitSubject.source}?`, "m");
+
+const commitPath = Bun.argv[2];
+
+if (!commitPath) {
+  throw new Error(`[ERROR]: Commit message path not provided.`);
+}
+
+/* Read commit from file */
+const commitFile = Bun.file(commitPath);
+const commitMessage = await commitFile.text();
+
+if (commitMessage.startsWith("@")) {
+  console.log("[LOG]: User activate bypass for this commit.");
+  console.log("[LOG]: Try to not use this feature and formalize\n commits according to the standard");
+  console.log(`[LOG]: Your commit: ${commitMessage}`);
+  /* Write commit to file without @ */
+  await commitFile.write(commitMessage.slice(1));
+}
+
+const matchTest = commitRegex.test(commitMessage);
+
+
+if (!matchTest) {
+  console.error(`[ERROR]: Invalid commit message -- ${commitMessage}\n Check our standard for commit messages.`);
+  process.exit(1);
+}
+
+type CommitInfo = {
+  type: string;
+  scope: string | undefined;
+  breaking: string | undefined;
+  subject: string;
+}
+
+const groups = commitRegex.exec(commitMessage)!.groups as CommitInfo;
+
+const info = Object
+  .entries(groups)
+  .map(([key, value]) => value ? `[LOG]: ${key}: ${value}` : "")
+  .filter(Boolean)
+  .join("\n");
+
+const output = `[LOG]: Commit pass git commit-msg hook. Commit message contains:\n${info}`;
+
+console.log(output);
